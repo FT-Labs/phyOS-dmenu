ce7049d300f51cb59a9942b46c35f3eeafa26aac
ardaPhysTech <phystecharda@gmail.com>
2022-01-24 00:32:50 +0300

    v1.0

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"SF Pro Text:style=Regular:size=20",
+	"SF Pro Text:style=Regular:size=18",
 	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = OPAQUE;
@@ -9,8 +9,8 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ededed", "#121212" },
-	[SchemeSel] = { "#121212", "#ededed" },
+	[SchemeNorm] = { "#e0ffff", "#121212" },
+	[SchemeSel] = { "#121212", "#e0ffff" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
@@ -22,7 +22,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 30;
+static unsigned int lineheight = 35;
 static unsigned int min_lineheight = 8;
 
 
