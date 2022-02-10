f592870cbf19a2e6a7b2159fd6b2cc1c9d13116b
ardaPhysTech <phystecharda@gmail.com>
2022-02-11 01:42:17 +0300

    v1.0

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"SF Pro Text:style=Regular:size=18",
@@ -9,8 +9,8 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e0ffff", "#121212" },
-	[SchemeSel] = { "#121212", "#e0ffff" },
+	[SchemeNorm] = { "#96cdfb", "#000000" },
+	[SchemeSel] = { "#1e1d2d", "#96cdfb" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
@@ -23,7 +23,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 static unsigned int lineheight = 36;
-static unsigned int min_lineheight = 36;
+static unsigned int min_lineheight = 8;
 
 
 /*
